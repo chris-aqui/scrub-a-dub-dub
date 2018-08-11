83eb5afe819667a34ca7cec50246256c0db7b46f
Christine <aqui.christine@gmail.com>
2018-08-10 21:17:10 -0400

    team work

--- scrub.js
+++ scrub.js
@@ -1,52 +1,62 @@
 const fs = require('fs');
 const output = [];
-const someData = {
-    name: '',
-    category: '',
-    sub_category: ''
+const RSVP = require('rsvp');
+var wasteData = require('./wasteTable.json');
+//declaring our class to create objects
+class ClassTrash {
+    constructor(name, category, sub_category) {
+        this.name = name;
+        this.category = category;
+        this.sub_category = sub_category;
+    }
 }
+//helper function that extracts any other titles in .title. eg: (title: 'sometitle ('someothertitle') -> 'someothertitle')
 const titleTransform = (aName) => {
     var dataTitle = aName.match(/\(([^)]*)\)/);
-    // console.log(" the title ", dataTitle);
     return dataTitle;
 }
-
+//transforms every single object from wasteTable (JSON array) into our desired classTrash objects
 const cleanUp = (rawData) => {
-    // console.log(rawData);
-    // console.log(typeof(rawData));
-
-        for (let dataItem of rawData) 
-        {
-            // console.log("******************************");
-            // console.log(dataItem.keywords);
-            // console.log("******************************");
-            let tempData = dataItem.keywords;
-            let tempArr = tempData.split(", ");
-            for( let i = 0; i < tempArr.length; i++){
-                someData.name = tempArr[i].toUpperCase();
-                someData.category = dataItem.category;
-                someData.sub_category = titleTransform(dataItem.title);
-                output.push(someData);
+    //forlooping every object
+    for (const dataItem of rawData) {
+        //creating a temporary array consisting of all the keywords in dataItem.keywords
+        let tempData = dataItem.keywords;
+        let tempArr = tempData.split(", ");
+        //looping over temporary array so we can create a new ClassTrash object for every single keyword
+        for (let i = 0; i < tempArr.length; i++) {
+            // checking if the item has an alternate name (paper towel & napkin)
+            // instantiate new ClassTrash object based of if else
+            if (titleTransform(dataItem.title) == null) {
+                var newTrash = new ClassTrash(tempArr[i].toUpperCase(), dataItem.category, 'no subcategory')
+            } else {
+                var newTrash = new ClassTrash(tempArr[i].toUpperCase(), dataItem.category, titleTransform(dataItem.title)[1])
             }
-        };
-    // return console.log(output);
+            // pushing to output array
+            // transforming our new ClassTrash object with JSON.stringify()
+            output.push(JSON.stringify(newTrash, null, 2));
+        }
+    };
 };
-
-
-fs.readFile('wasteTable.json', 'utf-8', function (err, buf) {
-    const bufObj = JSON.parse(buf);
-    // const bufObj = JSON.stringify(buf);
-    console.log('reading file');
-    // console.log(typeof(buf));
-    // console.log(bufObj);
-    // cleanUp(buf);
-    cleanUp(bufObj);
-    console.log('done');
+//executing all our code here:
+cleanUp(wasteData);
+var promise = new RSVP.Promise(function (fulfill, reject) {
+    var firstWriting = fs.writeFile('output.json', '[', function (err, data) {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+    fulfill(firstWriting);
 });
-
-
-
-fs.writeFile('output.json', output, function (err, data) {
-    if (err) console.log(err);
-    console.log("Successfully Written to File.");
-});
+//we're manually adding the square brackets to meet .json file requirement i.e: [{}, {}]
+//output does't write to output.json with square brackets
+//we add them ourselves by chaining promises
+promise.then(function () {
+    fs.appendFile('output.json', output, function (err, data) {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+}, null).then(function () {
+    fs.appendFile('output.json', ']', function (err, data) {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+}, null);
